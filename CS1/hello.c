f0a33ead2cee6f766109b3bb4239d59f6c0c23a3
talhaRehmann <kingtalha312@gmail.com>
2023-10-19 00:01:00 +0500

    name and date added

---
+++ CS1/hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: TALHA REHMAN
+ * Date: 2023/10/19
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
